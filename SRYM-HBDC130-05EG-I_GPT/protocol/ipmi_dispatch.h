d500d3ae1dcdcb3d41b4ae3bab08ef3f4ed7d1d0
tanzihuai666 <129679948+tanzihuai666@users.noreply.github.com>
2026-06-12 17:32:59 +0800

    Add IPMI dispatch header comments

--- SRYM-HBDC130-05EG-I_GPT/protocol/ipmi_dispatch.h
+++ SRYM-HBDC130-05EG-I_GPT/protocol/ipmi_dispatch.h
@@ -1,9 +1,19 @@
+/**
+ * @file    ipmi_dispatch.h
+ * @brief   IPMI 命令分发接口。
+ * @author  GPT
+ * @date    2026-06-12
+ * @version V0.3
+ */
 #ifndef IPMI_DISPATCH_H
 #define IPMI_DISPATCH_H
 
 #include "ipmb_frame.h"
 
+/** @brief 初始化 IPMI 数据库。@param own_addr_8bit 本机 IPMB 8-bit 地址。@param ga_id 槽位 ID。@retval 无。 */
 void ipmi_init(uint8_t own_addr_8bit, uint8_t ga_id);
+
+/** @brief 分发 IPMI 请求并填写响应。@param req 请求。@param rsp 响应。@retval 无。 */
 void ipmi_dispatch(const ipmi_request_t *req, ipmi_response_t *rsp);
 
 #endif
