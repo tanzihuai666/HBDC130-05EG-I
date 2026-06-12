6fb300178d13ebb0ecf19c1675845d3212ad46d4
tanzihuai666 <129679948+tanzihuai666@users.noreply.github.com>
2026-06-12 17:34:32 +0800

    Comment timer header

--- SRYM-HBDC130-05EG-I_GPT/driver/bsp_timer.h
+++ SRYM-HBDC130-05EG-I_GPT/driver/bsp_timer.h
@@ -1,10 +1,18 @@
+/* File: bsp_timer.h
+ * Module: 1ms tick interface.
+ */
 #ifndef BSP_TIMER_H
 #define BSP_TIMER_H
 
 #include <stdint.h>
 
+/* Init 1ms tick. */
 void bsp_timer_init_1ms(void);
+
+/* Return current ms count. */
 uint32_t bsp_millis(void);
+
+/* Hold CPU for ms count. */
 void bsp_delay_ms(uint32_t ms);
 
 #endif
