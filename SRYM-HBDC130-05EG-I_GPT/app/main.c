c952039d510ac9c8d89e784e3f0ae8a7dd2e8c47
tanzihuai666 <129679948+tanzihuai666@users.noreply.github.com>
2026-06-12 10:36:09 +0800

    Wire main loop to BSP layer

--- SRYM-HBDC130-05EG-I_GPT/app/main.c
+++ SRYM-HBDC130-05EG-I_GPT/app/main.c
@@ -3,21 +3,25 @@
 #include "sensor_manager.h"
 #include "fault_manager.h"
 #include "ipmi_dispatch.h"
-
-static volatile uint32_t g_ms_tick;
-
-void SysTick_Handler(void)
-{
-    g_ms_tick++;
-}
+#include "bsp_gpio.h"
+#include "bsp_timer.h"
+#include "bsp_adc_dma.h"
+#include "bsp_uart.h"
+#include "bsp_watchdog.h"
+#include "bsp_i2c.h"
 
 static void board_init(void)
 {
     SystemInit();
-    SysTick_Config(SystemCoreClock / 1000u);
+    bsp_timer_init_1ms();
+    bsp_gpio_init();
+    bsp_uart1_init(115200u);
+    bsp_adc_dma_init();
     power_ctrl_init();
     ipmi_init(power_ctrl_get_ipmb_addr_8bit(), power_ctrl_get_ga_id());
+    bsp_i2c1_ipmb_init((uint8_t)(power_ctrl_get_ipmb_addr_8bit() >> 1));
     fault_manager_init();
+    bsp_watchdog_init();
 }
 
 int main(void)
@@ -28,18 +32,24 @@ int main(void)
     board_init();
 
     while (1) {
+        uint32_t now = bsp_millis();
         power_ctrl_task_1ms();
 
-        if ((uint32_t)(g_ms_tick - last_10ms) >= 10u) {
-            last_10ms = g_ms_tick;
+        if ((uint32_t)(now - last_10ms) >= 10u) {
+            bsp_adc_values_t adc;
+            last_10ms = now;
+            bsp_adc_dma_task_10ms();
+            bsp_adc_get_values(&adc);
+            sensor_set_adc_values(adc.vin_v, adc.v12_v, adc.v5_v, adc.v33_v, adc.vm12_v, adc.v28_v,
+                                  adc.vin_i, adc.i12_a, adc.i33_a, adc.i5_a, adc.temp_c);
             fault_manager_task_10ms();
         }
 
-        if ((uint32_t)(g_ms_tick - last_100ms) >= 100u) {
-            last_100ms = g_ms_tick;
+        if ((uint32_t)(now - last_100ms) >= 100u) {
+            last_100ms = now;
             sensor_manager_task_100ms();
         }
 
-        /* 后续接入 ipmb_driver_task()：I2C1 Slave RX + Master TX Response 队列。 */
+        bsp_watchdog_feed();
     }
 }
