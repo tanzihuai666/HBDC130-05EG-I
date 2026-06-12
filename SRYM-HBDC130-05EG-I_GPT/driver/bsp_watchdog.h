b0716639d49a9686cf952db53afa70d007f8a902
tanzihuai666 <129679948+tanzihuai666@users.noreply.github.com>
2026-06-12 17:31:04 +0800

    Add watchdog header comments

--- SRYM-HBDC130-05EG-I_GPT/driver/bsp_watchdog.h
+++ SRYM-HBDC130-05EG-I_GPT/driver/bsp_watchdog.h
@@ -1,7 +1,17 @@
+/**
+ * @file    bsp_watchdog.h
+ * @brief   Independent watchdog interface.
+ * @author  GPT
+ * @date    2026-06-12
+ * @version V0.3
+ */
 #ifndef BSP_WATCHDOG_H
 #define BSP_WATCHDOG_H
 
+/** @brief Initialize IWDG. @param None. @retval None. */
 void bsp_watchdog_init(void);
+
+/** @brief Reload IWDG counter. @param None. @retval None. */
 void bsp_watchdog_feed(void);
 
 #endif
