e44490cb99bc06c64645e0c0e304b5f5ab562e0a
tanzihuai666 <129679948+tanzihuai666@users.noreply.github.com>
2026-06-12 14:25:48 +0800

    Integrate IPMB driver task in main loop

--- SRYM-HBDC130-05EG-I_GPT/app/main.c
+++ SRYM-HBDC130-05EG-I_GPT/app/main.c
@@ -3,6 +3,7 @@
 #include "sensor_manager.h"
 #include "fault_manager.h"
 #include "ipmi_dispatch.h"
+#include "ipmb_driver.h"
 #include "bsp_gpio.h"
 #include "bsp_timer.h"
 #include "bsp_adc_dma.h"
@@ -12,14 +13,20 @@
 
 static void board_init(void)
 {
+    uint8_t own_addr_8bit;
+
     SystemInit();
     bsp_timer_init_1ms();
     bsp_gpio_init();
     bsp_uart1_init(115200u);
     bsp_adc_dma_init();
     power_ctrl_init();
-    ipmi_init(power_ctrl_get_ipmb_addr_8bit(), power_ctrl_get_ga_id());
-    bsp_i2c1_ipmb_init((uint8_t)(power_ctrl_get_ipmb_addr_8bit() >> 1));
+
+    own_addr_8bit = power_ctrl_get_ipmb_addr_8bit();
+    ipmi_init(own_addr_8bit, power_ctrl_get_ga_id());
+    ipmb_driver_init(own_addr_8bit);
+    bsp_i2c1_ipmb_init((uint8_t)(own_addr_8bit >> 1));
+
     fault_manager_init();
     bsp_watchdog_init();
 }
@@ -34,6 +41,7 @@ int main(void)
     while (1) {
         uint32_t now = bsp_millis();
         power_ctrl_task_1ms();
+        ipmb_driver_task();
 
         if ((uint32_t)(now - last_10ms) >= 10u) {
             bsp_adc_values_t adc;
