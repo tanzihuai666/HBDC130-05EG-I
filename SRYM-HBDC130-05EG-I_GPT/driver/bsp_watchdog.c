723b6eef62a6ea6eaab7166501cce46873060980
tanzihuai666 <129679948+tanzihuai666@users.noreply.github.com>
2026-06-12 17:31:14 +0800

    Add watchdog comments and log

--- SRYM-HBDC130-05EG-I_GPT/driver/bsp_watchdog.c
+++ SRYM-HBDC130-05EG-I_GPT/driver/bsp_watchdog.c
@@ -1,6 +1,15 @@
+/**
+ * @file    bsp_watchdog.c
+ * @brief   Independent watchdog BSP.
+ * @author  GPT
+ * @date    2026-06-12
+ * @version V0.3
+ */
 #include "bsp_watchdog.h"
+#include "app_config.h"
 #include "gd32f10x.h"
 
+/** @brief Initialize IWDG to about 1s timeout. */
 void bsp_watchdog_init(void)
 {
     IWDG_Write_Enable(IWDG_WRITEACCESS_ENABLE);
@@ -8,8 +17,10 @@ void bsp_watchdog_init(void)
     IWDG_SetReloadValue(625u); /* About 1s at 40kHz/64. */
     IWDG_ReloadCounter();
     IWDG_Enable();
+    APP_LOGI("watchdog init");
 }
 
+/** @brief Feed IWDG; called only after main loop tasks return. */
 void bsp_watchdog_feed(void)
 {
     IWDG_ReloadCounter();
